580ee426e273ea3a63b5de9849efb4ce91ade116
BhSofware <70692211+BhSofware@users.noreply.github.com>
2020-09-26 22:36:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var kaas = prompt("Is de kaas geel?");
+
+if (kaas == "ja") {
+var gaten = prompt("zitten er gaten?");
+}
+else if (kaas == "nee") {
+var schimmels = prompt("Heeft de kaas blauwe schimmels?");
+}
+if (gaten == "ja") {
+var duur = prompt("is kaas belachelijk duur?");
+}
+else if(gaten == "nee") {
+var hard = prompt("Is de kaas hard als steen?");		
+}
+if (schimmels == "ja") {
+var korst = prompt("Heeft de kaas een korst?");
+}
+else if (schimmels == "nee"){
+var korst1 = prompt("Heeft de kaas een korst?");
+}
+if (duur == "ja"){
+alert("U denkt aan Emmenthaler");	
+}
+else if (duur == "nee") 
+alert("U denkt aan Leerdammer"); {
+}
+if (hard == "ja") {
+alert("U denkt aan Panmigiano Reggiano")
+}
+else if (hard == "nee") {
+alert("U denkt aan Goudse kaas");
+}
+if (korst == "ja") {
+alert("U denkt aan Foume d'Ambert");	
+}
+else if (korst == "nee") {
+alert("U denkt aan Camembert");
+}
+if (korst1 == "ja") {
+alert("U denkt aan mozzarella");
+}
+else if (korst1 == "nee") {
+	alert("U denkt aan Blue de Rochbaron");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
